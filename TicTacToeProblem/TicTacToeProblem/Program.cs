d0e8bcd281a79b0040ee8fda493133e51d91330f
unknown <Saitharala@gmail.com>
2021-12-05 16:21:31 +0530

    [TSP] Added Creating Board Feauture

---
+++ TicTacToeProblem/TicTacToeProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TicTacToeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TicTacToe Game:");
+            TicTacToe1 play = new TicTacToe1();
+            play.CreateBoard();
+
+        }
+    }
+}
---
+++ TicTacToeProblem/TicTacToeProblem/TicTacToe1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeProblem
+{
+    class TicTacToe1
+    {
+        public void CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
